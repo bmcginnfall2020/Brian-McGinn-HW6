2b67c09e372451341a6893186c698555516f1117
Brian McGinn <bmcginn@njcu.edu>
2020-09-18 19:42:12 +0000

    I made the program display why I'm taking CS407.

--- MyName.java
+++ MyName.java
@@ -1,5 +1,5 @@
 public class MyName{
 	public static void main(String args[]){
-		System.out.println("My favorite programming language is Javascript.");
+		System.out.println("I am taking CS407 because I might find it useful in the future.");
 	}
 }
