8473ab1a8792f0c1023fd60355d263be7fd6f0bf
Brian McGinn <bmcginn@njcu.edu>
2020-09-18 19:39:58 +0000

    The program will now display my favorite programming language.

--- MyName.java
+++ MyName.java
@@ -1,5 +1,5 @@
 public class MyName{
 	public static void main(String args[]){
-		System.out.println("My name is Brian.");
+		System.out.println("My favorite programming language is Javascript.");
 	}
 }
