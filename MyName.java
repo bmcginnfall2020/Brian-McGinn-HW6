d24a97e687e1b92f42aef8a0f04b6bb21c0c2295
Brian McGinn <bmcginn@njcu.edu>
2020-09-18 19:43:21 +0000

    I made the program display what my favorite food is.

--- MyName.java
+++ MyName.java
@@ -1,5 +1,5 @@
 public class MyName{
 	public static void main(String args[]){
-		System.out.println("I am taking CS407 because I might find it useful in the future.");
+		System.out.println("My favorite food is Chicken Parmesan.");
 	}
 }
